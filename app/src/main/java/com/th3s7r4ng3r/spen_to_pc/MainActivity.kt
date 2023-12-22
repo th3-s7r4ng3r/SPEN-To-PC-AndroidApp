c235eab6c80a3d0069e62aa8f366d8fa2710a812
VinuraGallage <th3-s7r4ng3r@users.noreply.github.com>
2023-12-22 21:50:29 +0530

    Add: Communication between platforms

--- app/src/main/java/com/th3s7r4ng3r/spen_to_pc/MainActivity.kt
+++ app/src/main/java/com/th3s7r4ng3r/spen_to_pc/MainActivity.kt
@@ -2,88 +2,121 @@ package com.th3s7r4ng3r.spen_to_pc
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.th3s7r4ng3r.spen_to_pc.ui.theme.SPENToPCTheme
-
-import android.os.AsyncTask
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.th3s7r4ng3r.spen_to_pc.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 import java.io.OutputStream
 import java.net.Socket
+
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var socket: Socket
     private lateinit var outputStream: OutputStream
+    private var isConnected = false // Track connection status
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        updateMainUI()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.connectButton.setOnClickListener {
-            val desktopIp = binding.desktopIpEditText.text.toString()
-            ConnectTask().execute(desktopIp)
+            connectToDesktop()
         }
-
         binding.singleClickButton.setOnClickListener {
-            SendDataTask().execute("Single Click")
+            sendData("Single Click")
         }
-
         binding.doubleClickButton.setOnClickListener {
-            SendDataTask().execute("Double Click")
+            sendData("Double Click")
         }
     }
 
-    @SuppressLint("StaticFieldLeak")
-    inner class ConnectTask : AsyncTask<String, Void, Boolean>() {
-        @Deprecated("Deprecated in Java")
-        @SuppressLint("SetTextI18n")
-        override fun doInBackground(vararg params: String): Boolean {
-            return try {
-                socket = Socket(params[0], 51515)
-                outputStream = socket.getOutputStream()
-                true
-            } catch (e: Exception) {
-                runOnUiThread {
-                    binding.connectionStatusLabel.text = "Connection Status: Failed"
-                }
-                false
-            }
+    @SuppressLint("SetTextI18n")
+    private fun connectToDesktop() {
+        if (isConnected) {
+            // Display error message if already connected
+            Toast.makeText(this, "Already connected to desktop", Toast.LENGTH_SHORT).show()
+            return
         }
 
-        @SuppressLint("SetTextI18n")
-        @Deprecated("Deprecated in Java")
-        override fun onPostExecute(result: Boolean) {
-            if (result) {
-                binding.connectionStatusLabel.text = "Connection Status: Connected"
+        // Disable the Connect button and display "Connecting..."
+        binding.connectionStatusLabel.text = "Connection Status: Connecting..."
+
+        val desktopIp = binding.desktopIpEditText.text.toString()
+        GlobalScope.launch(Dispatchers.IO) {
+            var connectSuccess = false
+            try {
+                    socket = Socket(desktopIp, 51515) // Create persistent socket
+                    outputStream = socket.getOutputStream()
+                    isConnected = true
+                    connectSuccess = true
+                    updateMainUI()
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    updateMainUI()
+                    binding.connectionStatusLabel.text = "Connection Status: Failed"
+                    Toast.makeText(this@MainActivity, "Connection failed: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            } finally {
+                withContext(Dispatchers.Main) {
+                    binding.connectionStatusLabel.text = "Connection Status: Disconnected"
+                }
             }
         }
     }
 
-    @SuppressLint("StaticFieldLeak")
-    inner class SendDataTask : AsyncTask<String, Void, Boolean>() {
-        @SuppressLint("SetTextI18n")
-        @Deprecated("Deprecated in Java")
-        override fun doInBackground(vararg params: String): Boolean {
-            return try {
-                val data = params[0]
-                outputStream.write(data.toByteArray())
-                true
+    private fun sendData(data: String) {
+        if (!isConnected) {
+            Toast.makeText(this, "Not connected to desktop", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                outputStream?.write(data.toByteArray())
             } catch (e: Exception) {
-                runOnUiThread {
-                    binding.connectionStatusLabel.text = "Connection Status: Failed"
+                // Switch to the main thread for UI interactions
+                isConnected = false
+                updateMainUI()
+                withContext(Dispatchers.Main) {
+                   Toast.makeText(this@MainActivity, "Error sending data: ${e.message}", Toast.LENGTH_SHORT).show()
                 }
-                false
             }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateMainUI(){
+            GlobalScope.launch(Dispatchers.IO) {
+                if(isConnected) {
+                    withContext(Dispatchers.Main) {
+                        binding.connectionStatusLabel.text = "Connection Status: Connected"
+                        binding.singleClickButton.isEnabled = true
+                        binding.doubleClickButton.isEnabled = true
+                    }
+                } else {
+                    withContext(Dispatchers.Main) {
+                        binding.connectionStatusLabel.text = "Connection Status: Disconnected"
+                        binding.singleClickButton.isEnabled = false
+                        binding.doubleClickButton.isEnabled = false
+                    }
+                }
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        try {
+            socket.close() // Close connection on app close
+        } catch (e: Exception) {
+            Toast.makeText(this, "Error closing the connection", Toast.LENGTH_SHORT).show()
         }
     }
 }
